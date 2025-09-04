7b5ff52e4c5993748064e69950b98e4d533c95ce
Dmytro Pavlusenko <138008557+Dima-deland@users.noreply.github.com>
2025-09-04 20:37:30 +0200

    Update MyTest.java
    test COVID less than 80

--- src/test/java/cicd/com/MyTest.java
+++ src/test/java/cicd/com/MyTest.java
@@ -13,9 +13,9 @@ public class MyTest {
         Calculator calc = new Calculator();
         assertEquals(5, calc.add(2, 3));
     }
-    @Test
-    public void testSubtract() {
-     Calculator calc = new Calculator();
-      assertEquals(1, calc.subtract(3, 2));
+  //  @Test
+  //  public void testSubtract() {
+  //   Calculator calc = new Calculator();
+  //    assertEquals(1, calc.subtract(3, 2));
    }
 }
