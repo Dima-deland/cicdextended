89e5138a77fc83584e73dedefae0f8fc972d478d
Dmytro Pavlusenko <pavlusenko.de@gmail.com>
2025-09-04 20:23:04 +0200

    Initial commit

---
+++ src/test/java/cicd/com/MyTest.java
@@ -0,0 +1,22 @@
+package cicd.com;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MyTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+
+//    @Test
+//    public void testSubtract() {
+//        Calculator calc = new Calculator();
+//        assertEquals(1, calc.subtract(3, 2));
+//    }
+}
